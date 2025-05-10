ecf810f07483aa2799271b121fe1c61beee50af5
williamjaysmith <williamjaysmith@gmail.com>
2025-05-10 11:05:00 -0500

    safe key

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -2,7 +2,7 @@ import { createClient } from "@supabase/supabase-js";
 
 export const supabaseUrl = "https://qaewrcsstqcsglmcemek.supabase.co";
 const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFhZXdyY3NzdHFjc2dsbWNlbWVrIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0NDc3MjI3NiwiZXhwIjoyMDYwMzQ4Mjc2fQ.BwpOaCenUF3jwXBseZVfo1NrpWNNJ1G3-mJPnnj85bY";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFhZXdyY3NzdHFjc2dsbWNlbWVrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ3NzIyNzYsImV4cCI6MjA2MDM0ODI3Nn0.2QjvQyrAsj4Tmvm7oEegA206t7OQotNEHlQLy8NtAQo";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
